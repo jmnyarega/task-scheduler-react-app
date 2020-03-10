14a49745ee60eb0d63ac87dc89cabc00a18c9d41
hjkl <jmnyarega@gmail.com>
2020-03-10 13:32:29 +0300

    setup

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+const Index = () => {
+  return (
+    <div>Start Coding</div>
+  )
+}
+
+ReactDom.render(<Index/>, document.getElementById("root"));
